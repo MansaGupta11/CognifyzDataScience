9cfb0052ebc366ce1c396e8596bc8b2e411148bf
Mansa Gupta <mansagupta2004@gmail.com>
2025-06-03 21:42:01 +0530

    level 3 task 2

--- level3Task2.py
+++ level3Task2.py
@@ -22,33 +22,4 @@ filtered_df = df.groupby('Cuisines').filter(lambda x: x['Votes'].sum() > 1000)
 top_cuisines_by_rating = filtered_df.groupby('Cuisines')['Aggregate rating'].mean().sort_values(ascending=False)
 print("Cuisines with Highest Avg Ratings (with >1000 votes):\n", top_cuisines_by_rating.head(10))
 
-#  Optional Visualization - Top Rated Cuisines
-plt.figure(figsize=(10,5))
-sns.barplot(
-    x=top_cuisines_by_rating.head(10).values,
-    y=top_cuisines_by_rating.head(10).index,
-    hue=top_cuisines_by_rating.head(10).index,  # Assign y as hue
-    palette="coolwarm",
-    legend=False  # Hide legend since it's redundant
-)
-plt.title("Top 10 Cuisines by Average Rating (min 1000 votes)")
-plt.xlabel("Average Rating")
-plt.ylabel("Cuisine")
-plt.tight_layout()
-plt.show()
 
-
-#  Optional Visualization - Most Popular Cuisines by Votes
-plt.figure(figsize=(10,5))
-sns.barplot(
-    x=cuisine_votes.head(10).values,
-    y=cuisine_votes.head(10).index,
-    hue=cuisine_votes.head(10).index,
-    palette="mako",
-    legend=False
-)
-plt.title("Top 10 Most Popular Cuisines by Total Votes")
-plt.xlabel("Total Votes")
-plt.ylabel("Cuisine")
-plt.tight_layout()
-plt.show()
