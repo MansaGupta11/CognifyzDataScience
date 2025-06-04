ca305e8d83b58dccbac1dbdd274e03befb332d85
Mansa Gupta <mansagupta2004@gmail.com>
2025-06-04 21:44:16 +0530

    modify

--- level1Task2.py
+++ level1Task2.py
@@ -24,25 +24,3 @@ print(df['City'].value_counts().head(10))
 print("\n Top 10 Most Common Cuisines:\n")
 print(df['Cuisines'].value_counts().head(10))
 
-# 3. Visualizations
-
-# Plot - Top 10 Cities
-top_cities = df['City'].value_counts().head(10)
-plt.figure(figsize=(10,5))
-sns.barplot(x=top_cities.index, y=top_cities.values, palette='viridis')
-plt.title("Top 10 Cities with Most Restaurants")
-plt.xlabel("City")
-plt.ylabel("Number of Restaurants")
-plt.xticks(rotation=45)
-plt.tight_layout()
-plt.show()
-
-# Plot - Top 10 Cuisines
-top_cuisines = df['Cuisines'].value_counts().head(10)
-plt.figure(figsize=(10,5))
-sns.barplot(y=top_cuisines.index, x=top_cuisines.values, palette='coolwarm')
-plt.title("Top 10 Most Common Cuisines")
-plt.xlabel("Number of Restaurants")
-plt.ylabel("Cuisine")
-plt.tight_layout()
-plt.show()
